3c44f4a35a2b8cc3cfb81f0f3ee645825c8f652a
cveleBaja <cvetkovicmilan1994@gmail.com>
2022-10-21 12:57:15 +0200

    add ui for  emails activity

---
+++ app/src/main/java/com/marjanskidusan/myemailclient/presentation/ui/emails/EmailsActivity.kt
@@ -0,0 +1,13 @@
+package com.marjanskidusan.myemailclient.presentation.ui.emails
+
+import android.os.Bundle
+import com.marjanskidusan.myemailclient.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EmailsActivity: BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
